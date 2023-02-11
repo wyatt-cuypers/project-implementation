a68e85cd22f56736903340a1de198337ce5246b5
Zakttack <zak.merrigan@ndsu.edu>
2023-02-11 16:33:14 -0600

    Created Nuget Package for Npgsql

---
+++ NAUCountryA/Service.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+namespace NAUCountryA
+{
+    public class Service
+    {
+        public static NpgsqlConnection Connection
+        {
+            get
+            {
+                string connectionString = "Server=localhost;Port=2023;User Id=postgres;" 
+                    + "Password=naucountrydev;Database=NAUCountryData";
+                NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+                connection.Open();
+                connection.Close();
+                return connection;
+            }
+        }
+    }
+}
