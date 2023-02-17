149bbe1f586bea906c480e777594cb3304fd9b87
Zakttack <zak.merrigan@ndsu.edu>
2023-02-17 15:26:40 -0600

    Created utility functions for the Data tables

--- NAUCountryA/Service.cs
+++ NAUCountryA/Service.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualBasic.FileIO;
+using NAUCountryA.Exceptions;
+using NAUCountryA.Models;
 using Npgsql;
-using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 namespace NAUCountryA
 {
     public class Service
@@ -10,7 +13,7 @@ namespace NAUCountryA
             get
             {
                 string sqlCommand = "";
-                string filePath = Service.InitialPathLocation + "\\NAUCountryA\\Resources\\database_construction.sql";
+                string filePath = InitialPathLocation + "\\NAUCountryA\\Resources\\database_construction.sql";
                 TextFieldParser sqlParcer = new TextFieldParser(filePath);
                 while (!sqlParcer.EndOfData)
                 {
@@ -19,13 +22,60 @@ namespace NAUCountryA
                 return sqlCommand;
             }
         }
-
         public static string InitialPathLocation
         {
             get
             {
                 return GetInitialPathLocation(Path.GetFullPath("."));
             }
+        }
+
+        public static string GetCreateTableSQLCommand(string tableName)
+        {
+            string sqlCommand = "";
+            string filePath = InitialPathLocation + "\\NAUCountryA\\Resources\\" + tableName.ToLower() + "_construction.sql";
+            TextFieldParser sqlParcer = new TextFieldParser(filePath);
+            while (!sqlParcer.EndOfData)
+            {
+                sqlCommand += sqlParcer.ReadLine();
+            }
+            return sqlCommand;
+        }
+        public static DataTable GetDataTable(string sqlCommand, User user)
+        {
+            DataTable table = new DataTable();
+            NpgsqlCommand cmd = new NpgsqlCommand(sqlCommand, user.Connection);
+            DbDataAdapter adapter = new NpgsqlDataAdapter(cmd);
+            string command = sqlCommand.Substring(0, sqlCommand.IndexOf(' ')).ToUpper();
+            switch (command)
+            {
+                case "DELETE":
+                    adapter.DeleteCommand = cmd;
+                    adapter.DeleteCommand.Connection.Open();
+                    adapter.Update(table);
+                    adapter.DeleteCommand.Connection.Close();
+                    break;
+                case "INSERT":
+                    adapter.InsertCommand = cmd;
+                    adapter.InsertCommand.Connection.Open();
+                    adapter.Update(table);
+                    adapter.InsertCommand.Connection.Close();
+                    break;
+                case "SELECT":
+                    adapter.SelectCommand = cmd;
+                    adapter.SelectCommand.Connection.Open();
+                    adapter.Fill(table);
+                    adapter.SelectCommand.Connection.Close();
+                    break;
+                case "UPDATE":
+                    adapter.UpdateCommand = cmd;
+                    adapter.UpdateCommand.Connection.Open();
+                    adapter.Update(table);
+                    adapter.UpdateCommand.Connection.Close();
+                    break;
+                default: throw new UnrecognizedSQLCommandException("The command isn't recognized in PostgreSQL.");
+            }
+            return table;
         }
 
         public static ICollection<string> ToCollection(string csvFileName)
