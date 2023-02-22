cd78667cc498669f8fa703e74b909c2478a5e4b6
Wyatt Cuypers <lawrence.cuypers@ndus.edu>
2023-02-21 20:18:40 -0600

    Added code for county and practice database tables

--- NAUCountryA/Models/County.cs
+++ NAUCountryA/Models/County.cs
@@ -1,12 +1,85 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NAUCountryA.Tables;
+using System.Data;
 
 namespace NAUCountryA.Models
 {
-    public class County
+    public class County : IEquatable<Commodity>
     {
-        
+        public County(int countyCode, int stateCode, string countyName, string recordTypeCode)
+        {
+            CountyCode = countyCode;
+            StateCode = stateCode;
+            CountyName = countyName;
+            IReadOnlyDictionary<string,RecordType> recordTypeEntries = new RecordTypeTable();
+            RecordType = recordTypeEntries[recordTypeCode];
+        }
+
+        public Commodity(DataRow row)
+        :this((int)row["COUNTY_CODE"], (int)row["STATE_CODE"], (string)row["COUNTY_NAME"], (string)row["RECORD_TYPE_CODE"])
+        {
+        }
+
+        public int CountyCode
+        {
+            get;
+            private set;
+        }
+
+        public int StateCode
+        {
+            get;
+            private set;
+        }
+
+        public string CountyName
+        {
+            get;
+            private set;
+        }
+
+        public RecordType RecordType
+        {
+            get;
+            private set;
+        }
+
+        public KeyValuePair<int,County> Pair
+        {
+            get
+            {
+                return new KeyValuePair<int,County>(CountyCode, this);
+            }
+        }
+
+        public bool Equals(County other)
+        {
+            return CountyCode == other.CountyCode && StateCode == other.StateCode &&
+            CountyName == other.CountyName && RecordType == other.RecordType;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static bool operator== (County a, County b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator!= (County a, County b)
+        {
+            return !a.Equals(b);
+        }
     }
 }
--- NAUCountryA/Models/Practice.cs
+++ NAUCountryA/Models/Practice.cs
@@ -1,12 +1,101 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using NAUCountryA.Tables;
+using System.Data;
 
 namespace NAUCountryA.Models
 {
-    public class Practice
+    public class Practice : IEquatable<Practice>
     {
-        
+        public Practice(int practiceCode, string practiceName, string practiceAbbreviation, int commodityCode, string releasedDate, string recordTypeCode)
+        {
+            PracticeCode = practiceCode;
+            PracticeName = practiceName;
+            PracticeAbbreviation = practiceAbbreviation;
+            CommodityCode = commodityCode;
+            ReleasedDate = releasedDate;
+            IReadOnlyDictionary<string,RecordType> recordTypeEntries = new RecordTypeTable();
+            RecordType = recordTypeEntries[recordTypeCode];
+        }
+
+        public Practice(DataRow row)
+        :this((int)row["PRACTICE_CODE"], (string)row["PRACTICE_NAME"], (string)row["PRACTICE_ABBREVIATION"], (int)row["COMMODITY_CODE"], (DateTime)row["RELEASED_DATE"], (string)row["RECORD_TYPE_CODE"])
+        {
+        }
+
+        public int PracticeCode
+        {
+            get;
+            private set;
+        }
+
+        public string PracticeName
+        {
+            get;
+            private set;
+        }
+
+        public string PracticeAbbreviation
+        {
+            get;
+            private set;
+        }
+
+        public int CommodityCode
+        {
+            get;
+            private set;
+        }
+
+        public string ReleasedDate
+        {
+            get;
+            private set;
+        }
+
+        public RecordType RecordType
+        {
+            get;
+            private set;
+        }
+
+        public KeyValuePair<int,Practice> Pair
+        {
+            get
+            {
+                return new KeyValuePair<int,Practice>(PracticeCode, this);
+            }
+        }
+
+        public bool Equals(Practice other)
+        {
+            return PracticeCode == other.PracticeCode && PracticeName == other.PracticeName &&
+            PracticeAbbreviation == other.PracticeAbbreviation && CommodityCode == other.CommodityCode &&
+            ReleasedDate == other.ReleasedDate && RecordType == other.RecordType;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static bool operator== (Practice a, Practice b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator!= (Practice a, Practice b)
+        {
+            return !a.Equals(b);
+        }
     }
 }
