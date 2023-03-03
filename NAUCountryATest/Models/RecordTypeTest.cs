a665faa8bd0f57545e904df5d5da5386fb71b7a0
Zakttack <zak.merrigan@ndsu.edu>
2023-03-03 16:42:37 -0600

    RecordType Model Works

--- NAUCountryATest/Models/RecordTypeTest.cs
+++ NAUCountryATest/Models/RecordTypeTest.cs
@@ -13,6 +13,27 @@ namespace NAUCountryATest.Models
         }
 
         [Test]
+        public void TestEquals1()
+        {
+            object other = new RecordType("A00420", 1, 2023);
+            Assert.That(recordType.Equals(other), Is.True);
+        }
+
+        [Test]
+        public void TestEquals2()
+        {
+            object other = new RecordType("B00420", 1, 2023);
+            Assert.That(recordType.Equals(other), Is.False);
+        }
+
+        [Test]
+        public void TestEquals3()
+        {
+            object other = "A00420";
+            Assert.That(recordType.Equals(other), Is.False);
+        }
+
+        [Test]
         public void TestToString()
         {
             string expected = "\"A00420\",\"01\",\"2023\"";
