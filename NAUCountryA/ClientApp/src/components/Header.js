86a2694cc7cfe7bc0cba39b8e28bd2daf9fff3cc
mirandaryan <miraryan21@gmail.com>
2023-04-01 21:53:51 -0500

    react app start

---
+++ NAUCountryA/ClientApp/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Header = () => {
+
+    const headerStyle = {
+
+        width: '100%',
+        padding: '2%',
+        backgroundColor: "lightblue",
+        color: 'black',
+        textAlign: 'left'
+    }
+
+    return (
+        <div style={headerStyle}>
+            <h1>Revenue Prices</h1>
+           
+        </div>
+
+    )
+}
---
+++ NAUCountryA/Controllers/FilterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NAUCountryA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilterController : ControllerBase
+    {
+        
+
+        private readonly ILogger<FilterController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
