117bd1deb3000496f5a92807f04c83248f46c3ed
droisum <104651792+droisum@users.noreply.github.com>
2023-04-10 17:49:45 -0500

    placeholder image in pdfs, cleanup

--- NAUCountryA/Program.cs
+++ NAUCountryA/Program.cs
@@ -2,57 +2,16 @@ using System;
 using NAUCountry.ECOMap;
 using NAUCountry.ECOMap.Models;
 
-// Code Review Comment:  This turned into a console application which is perfectly fine but it
-// might have been good to create a project for your PDf generation code and then referenced that
-// project between the service API you will tie into and the created a console application project (like I did with the map example)
-// that does just this (load tables, output result).  Now you have a mix of a console app and API app and it might get very weird
-// turning this back into an API.  Keeping things simple and broken out helps with consolidation steps in the future.
-//
-// Same with unit tests as your unit test project references the NAUCountryA application.  By breaking out the core logic into a nother project
-// you can setup unit tests to the core logic and not bring with it the whole application.
-//
-// I will try to show an example of this.
+/*LoadingProcessor loader = new LoadingProcessor(CsvUtility.InitialPathLocation);
+ECODataService service = await loader.LoadAll();
 
-// DESIGN NOTE / Code Review: having separating out the code into a new project you could easily create a
-// console application to test this while reverting this back to the web application needed
-try
-{
-    LoadingProcessor loader = new LoadingProcessor(CsvUtility.InitialPathLocation);
-    ECODataService service = await loader.LoadAll();
+Price testPrice = service.PriceEntries.First().Value;
+EcoPdfGenerator.GeneratePDF(service, testPrice.Offer.County.State, testPrice.Offer.Practice.Commodity, 2023);
 
-    Price testPrice = service.PriceEntries.First().Value;
-    EcoPdfGenerator.GeneratePDF(service, testPrice.Offer.County.State, testPrice.Offer.Practice.Commodity, 2023);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.ToString());
-}
-//CreatePDF.Run();
-//Service.ConstructUser();
-// ICollection<string> commodityDataSet = Service.ToCollection("A23_Commodity");
-// Console.WriteLine(commodityDataSet.Count);
-// ICollection<int> commodityIds = new HashSet<int>();
-// IEnumerator<string> commodityEnum = commodityDataSet.GetEnumerator();
-// if (commodityEnum.MoveNext())
-// {
-//     string[] headers = commodityEnum.Current.Split(',');
-//     while (commodityEnum.MoveNext())
-//     {
-//         string[] values = commodityEnum.Current.Split(',');
-//         commodityIds.Add(Convert.ToInt32(values[4]));
-//     }
-//     Console.WriteLine(commodityIds.Count);
-// }
-// string sqlCommand = $"SELECT * FROM public.\"State\" WHERE \"STATE_CODE\" = 01;";
-// System.Data.DataTable table = Service.GetDataTable(sqlCommand);
-// Service.GeneratePDF(new State(table.Rows[0]));
-
-Console.WriteLine("PDF Generated.");
-// Price testPrice = Service.PriceEntries.Values.First();
-// Service.GeneratePDF(testPrice.Offer.State, testPrice.Offer.Practice, testPrice.Offer.Type);
+Console.WriteLine("PDF Generated.");*/
 
 
-/*var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -78,4 +37,4 @@ app.MapControllerRoute(
 
 app.MapFallbackToFile("index.html"); ;
 
-app.Run();*/
+app.Run();
