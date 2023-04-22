7a07bcdf3896a92a5db1b704d5bae15310bc47fd
droisum <104651792+droisum@users.noreply.github.com>
2023-04-22 12:30:34 -0500

    extent fix

--- ECOMap/ESRIClient.cs
+++ ECOMap/ESRIClient.cs
@@ -48,8 +48,7 @@ namespace ECOMap
                 String stateCode = EcoGeneralService.RemoveQuotationsFromCurrentFormat(state.FormatStateCode());
                 UpdateUniqueValueInfos((JObject)(obj["operationalLayers"]?[0]?["layerDefinition"]?["drawingInfo"]?["renderer"]));
                 obj["operationalLayers"][0]["layerDefinition"]["definitionExpression"] = $"CNT_STATE_ = '{stateCode}'";
-                obj["mapOptions"]["extent"].Remove();
-                obj["mapOptions"]["extent"] = Extent;
+                obj["mapOptions"]["extent"] = Extent["extent"];
                 Console.WriteLine(obj);
                 return obj;
             }
